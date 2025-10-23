33407bcb3765a62306bc34f16ee582fe96cd80d2
pope-h <hamatthan@gmail.com>
2025-10-23 19:33:24 +0100

    added embedded endpoint

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3005;
 
-const allowedOrigins = ["http://localhost:3000", "https://craftlink-hq.vercel.app", "https://craftlink-alpha.vercel.app", "https://craftlinkhq.com", "https://www.craftlinkhq.com"];
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://craftlink-hq.vercel.app",
+  "https://craftlink-alpha.vercel.app",
+  "https://craftlinkhq.com",
+  "https://www.craftlinkhq.com",
+  "https://embedded-wallet.thirdweb.com",
+];
 app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
